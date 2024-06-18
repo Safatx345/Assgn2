6b192c2bb43f7fa5904daccca849e00a26aff093
Safat Sadnan Snehal <sf231351@dal.ca>
2024-06-18 14:32:36 -0300

    Setting Up database and adding the entities

---
+++ backend/src/main/java/com/csci3130/assignment_2_demo/service/Implementations/ResumeServiceImpl.java
@@ -0,0 +1,33 @@
+package com.csci3130.assignment_2_demo.service.Implementations;
+
+import com.csci3130.assignment_2_demo.model.Resume;
+import com.csci3130.assignment_2_demo.repository.ResumeRepository;
+import com.csci3130.assignment_2_demo.service.ResumeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class ResumeServiceImpl implements ResumeService {
+
+    @Autowired
+    ResumeRepository resumeRepository;
+
+    @Override
+    public String createResume(Resume resume) {
+        resumeRepository.save(resume);
+        return "Resume created";
+    }
+
+    @Override
+    public List<Resume> getResume(String role) {
+        return resumeRepository.findByRole(role);
+    }
+
+
+
+
+}
---
+++ backend/src/test/java/com/csci3130/assignment_2_demo/Assignment2DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csci3130.assignment_2_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assignment2DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
